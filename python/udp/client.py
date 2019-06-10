7512ddda4e85d23ca74caf1f825eb5887a2991c6
Bianca Santiago <bianca.santiago72@gmail.com>
2019-06-10 14:14:33 -0300

    comentando codigo dos arquivos de protocolo udp em python

---
+++ python/udp/client.py
@@ -0,0 +1,43 @@
+# Importando a biblioteca de socket
+import socket 
+
+# Importando a biblioteca de datetime
+from datetime import datetime
+
+# Utilizando o parametro datetime.now 
+# para pegar a hora do SO
+hora_atual = datetime.now()
+
+# Utilizando o parametro strftime 
+# para personalizar o formato da hora
+view_hora = hora_atual.strftime('%H:%M')              
+ 
+# 1. CRIACAO DO SOCKET
+
+# Criando um objeto socket 
+s = socket.socket()        
+
+# 2. LIGACAO (BIND) DOS ENDERECOS COM A PORTA
+ 
+# Definindo a porta a qual o cliente quer se conectar
+# Nesse caso 12345 (server)
+port = 12345               
+ 
+# Conectando-se ao servidor no computador local
+s.connect(('127.0.0.1', port))
+
+# 3. ENVIANDO DADOS
+
+# Enviando dados ao servidor
+input_string = raw_input("Enter data you want to send->")
+s.sendall('Mensagem ' + input_string + ' recebida as ' + view_hora)
+
+# 4. RECEBENDO DADOS
+
+# Recebendo dados do servidor
+print s.recv(1024)
+
+# 5. ENCERRANDO A CONEXAO
+
+# Encerrando a conexao com o servidor
+s.close()  
---
+++ python/udp/server.py
@@ -0,0 +1,56 @@
+# Importando a biblioteca de socket
+import socket 
+
+# 1. CRIACAO DO SOCKET 
+
+# Criando um objeto socket
+s = socket.socket()                 
+print "socket criado com sucesso"   
+ 
+# Reservando uma porta no computador local
+# nesse caso, a porta eh 12345
+port = 12345      
+
+# 2. LIGACAO (BIND) DOS ENDERECOS COM A PORTA
+
+# Campo IP vazio
+# Servidor ouvindo pedidos providos de 
+# outros computadores na rede
+s.bind(('', port))        
+print "socket ligado a %s" %(port)
+
+# Colocando o socket em modo escuta
+s.listen(5)     
+print "socket esta ouvindo a rede"      
+
+# 3. RECEBENDO DADOS
+
+# LOOP INFINITO PARA LEITURA (LISTEN)
+while True:
+ 
+   # Esteblecendo conexao com o cliente.
+   c, addr = s.accept()     
+   print 'Got connection from', addr
+
+   # Pegando dados do cliente
+   data=c.recv(1024)
+   
+   a=""
+
+   for i in range(0,len(data)):
+      a=a+data[i]
+
+   if not data:
+     break
+
+   # 4. ENVIANDO DADOS PARA O CLIENTE
+   
+   # Imprimindo a mensagem recebida do cliente
+   print a
+
+   # 5. ENCERRANDO A CONEXAO
+ 
+   # Encerrando a conexao com o cliente
+   c.close()
+
+# FIM DO LOOP INFINITO
